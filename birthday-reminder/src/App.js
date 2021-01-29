4919bc67a37d93d35ceaceb28d2d4cf9ef907f8a
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-29 01:07:04 +0000

    creating component

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
+import data from './data'
+import List from './ListComponent'
+import './index.css'
+import { useState } from 'react'
 const App = () => {
+  const [people, updatePeople] = useState(data)
   return (
-    <h1>Hello world</h1>
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday Today</h3>
+        <List peopleList={people} />
+        <button onClick={() => updatePeople([])}>Clear All</button>
+      </section>
+    </main>
   )
 }
-
 export default App;
